ad1763b45300802850e62cc5edb62c0f1400d450
igorBys <stevejobs69@MacBook-Air-steve.local>
2025-03-16 17:08:27 +0300

    add GitHubEnterprizeTest and build.gradle

---
+++ src/test/java/GitHubEnterprizeTest.java
@@ -0,0 +1,45 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubEnterprizeTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void testNavigateToEnterpriseSolutions() {
+        open("https://github.com");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+    }
+
+    @Test
+    void testDragAndDropWithActions() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(150, 0).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        sleep(5000);
+    }
+    @Test
+    void testDragAndDropWithMethod() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        sleep(5000);
+    }
+}
